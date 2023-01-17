a0e4fe04d213979d1c30d8d9ad1634ce9c3a5c80
AltmyshovNursultan <60.nursultank@gmail.com>
2023-01-17 08:28:32 +0100

    Initial commit

---
+++ src/test/java/delivery/stork/StorkApplicationTests.java
@@ -0,0 +1,13 @@
+package delivery.stork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
